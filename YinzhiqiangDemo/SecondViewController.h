0cdb577dad431518aef944501ed53c46a52365f8
yinzhiqiang19 <yinzhiqiang19@163.com>
2016-07-26 14:00:19 +0800

    添加viewController
    添加viewcontroller

---
+++ YinzhiqiangDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  YinzhiqiangDemo
+//
+//  Created by yinzhiqiang on 16/7/26.
+//  Copyright © 2016年 ubt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
